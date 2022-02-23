eaebd09756548e62dcc6fc22b8f6698aa093ed17
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-02-23 12:46:13 +0530

    structure added

---
+++ Public/scripts/products.js
@@ -0,0 +1,405 @@
+//Array Of Objects
+
+var arr =
+  [
+    {
+      "id": "1",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Azure8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Azure9_1800x1800.jpg?v=1636735906",
+      "title": "Luft Azure Blue Melange",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "2",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Cocoa8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Cocoa9_1800x1800.jpg?v=1636735954",
+      "title": "Luft Cocoa Brown",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "3",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Royal8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Royal9_1800x1800.jpg?v=1636736087",
+      "title": "Luft Royal Blue",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "4",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Fern8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Fern9_1800x1800.jpg?v=1636736017",
+      "title": "Luft Fern Green",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "5",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Shadow8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Shadow9_1800x1800.jpg?v=1636736091",
+      "title": "Luft Shadow Grey",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "6",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Crimson8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/Crimson9_1800x1800.jpg?v=1636735990",
+      "title": "Luft Crimson Red",
+      "original_price": "3999",
+      "final_price": "3329",
+      "offer": "Save 17%"
+    },
+    {
+      "id": "7",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/BlackOnBlack2_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/BlackOnBlack8_1800x1800.jpg?v=1631782732",
+      "title": "Drift Black On Black",
+      "original_price": "1440",
+      "final_price": "1299",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "8",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/GreyonBlack2_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/GreyonBlack8_1800x1800.jpg?v=1631782809",
+      "title": "Drift Grey On Black",
+      "original_price": "1440",
+      "final_price": "1299",
+      "offer": "Save 11%"
+
+    },
+    {
+      "id": "9",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/GreyonWhite3_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/GreyonWhite8_1800x1800.jpg?v=1631782852",
+      "title": "Drift Grey On White",
+      "original_price": "1440",
+      "final_price": "1299",
+      "offer": "Save 11%"
+
+    },
+    {
+      "id": "10",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/BlueOnWhite2_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/BlueOnWhite8_1800x1800.jpg?v=1631782838",
+      "title": "Drift Navy On White",
+      "original_price": "1440",
+      "final_price": "1299",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "11",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/10_61cce8ed-2c8f-4ea5-8d2a-7702237ddb84_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/2_6f7303b8-63a3-4265-a65f-6a3d72054e4a_1800x1800.jpg?v=1634022342",
+      "title": "New Troos Red Melange",
+      "original_price": "2995",
+      "final_price": "2665 ",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "12",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/10_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/2_1800x1800.jpg?v=1634021934",
+      "title": "New Troos Black Melange",
+      "original_price": "2995",
+      "final_price": "2665",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "13",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/10_eb54c85e-9359-445f-81c3-7685f1860da8_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/2copy_1800x1800.jpg?v=1634022065",
+      "title": "New Troos Blue Melange",
+      "original_price": "2995",
+      "final_price": "2665 ",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "14",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/10_7c0e4a36-4d46-44d6-810b-85d429ace1d4_1800x1800.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/2_d0c82600-4df6-423c-b76f-e54099551b4f_1800x1800.jpg?v=1634022154",
+      "title": "New Troos Grey Melange",
+      "original_price": "2995",
+      "final_price": "2665 ",
+      "offer": "Save 11%"
+    },
+    {
+      "id": "15",
+      "image1": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/10_c5897a5e-0a53-4df8-96b8-6fc531a396b4_540x.jpg",
+      "image2": "https://cdn.shopify.com/s/files/1/0258/2485/4100/products/2_af1ff848-d660-498b-b62b-80effdb584f2_1800x1800.jpg?v=1634022252",
+      "title": "New Troos Green Melange",
+      "original_price": "2995",
+      "final_price": "2665",
+      "offer": "Save 11%"
+    }
+  ]
+
+
+
+let products1 = document.getElementById("products");
+
+// let data = arr.products;
+
+displayProducts(arr);
+
+
+//Price Sorting
+function handlePricesort() {
+
+  let selected = document.getElementById("priceSort").value;
+  if (selected == "low") {
+    arr.sort(function (a, b) {
+      return Number(a.final_price) - Number(b.final_price)
+      // console.log(a.final_price)
+
+    });
+  } if (selected == "high") {
+    arr.sort(function (a, b) {
+      return Number(b.final_price) - Number(a.final_price)
+    });
+  }
+  displayProducts(arr);
+}
+
+
+//Name Sorting
+function handleNamesort() {
+
+  var selected = document.querySelector("#nameSort").value;
+  if (selected == "asc") {
+    arr.sort(function (a, b) {
+      if (a.title > b.title) return 1;
+      if (a.title < b.title) return -1;
+      return 0;
+    });
+  }
+  if (selected == "dsc") {
+    arr.sort(function (a, b) {
+      if (a.title > b.title) return -1;
+      if (a.title < b.title) return 1;
+      return 0;
+    });
+  }
+
+  displayProducts(arr);
+
+}
+
+
+//function for display the products
+function displayProducts(data) {
+
+  products1.textContent = "";
+
+  data.map((el) => {
+
+    let div = document.createElement("div");
+
+    div.addEventListener("click", () => {
+      productsAdded(el)
+    })
+
+    var img1 = document.createElement("img");
+    let start = img1.src = el.image2;
+    let hover = img1.src = el.image1;
+
+    //movesover images 
+    img1.onmouseover = () => {
+      img1.src = start;
+    }
+    img1.onmouseout = () => {
+      img1.src = hover;
+    }
+
+    let name = document.createElement("h3");
+    name.textContent = el.title;
+
+    let sub_div = document.createElement("div");
+    sub_div.setAttribute("id", "sub_div");
+
+    let ori_price = document.createElement("p");
+    ori_price.textContent = `₹ Rs.${el.original_price}`;
+    ori_price.style.textDecoration = "line-through"
+
+    let fin_price = document.createElement("p");
+    fin_price.textContent = `₹ Rs. ${el.final_price}`;
+
+    let off = document.createElement("p");
+    off.textContent = el.offer;
+    off.style.color = "tomato";
+
+    sub_div.append(ori_price, fin_price, off);
+
+    div.append(img1, name, sub_div);
+    products1.append(div);
+  });
+}
+
+//setting Items in localStorage
+
+function productsAdded(el) {
+  var addedProducts = [];
+
+  addedProducts.push(el)
+  console.log(addedProducts)
+  localStorage.setItem("myProducts", JSON.stringify(addedProducts));
+
+  window.location.href = "../checkout/Productpage.html";
+}
+
+//CheckBox filter
+
+//luft Checkbox Filter
+document.getElementById("luft").addEventListener("change", myLuft);
+
+function myLuft() {
+  let luft = document.getElementById("luft").checked;
+
+  if (luft === true) {
+    let luft_products = arr.filter(function (el) {
+      var k = el.title;
+      for (var i = 0; i < k.length; k++) {
+        if (k[i] + k[i + 1] + k[i + 2] + k[i + 3] === "Luft") {
+          return el;
+        }
+      }
+    })
+    displayProducts(luft_products);
+  }
+  else {
+    displayProducts(arr);
+  }
+}
+
+//Drift Checkbox Filter
+document.getElementById("drift").addEventListener("change", myDrift);
+
+function myDrift() {
+
+  let drift = document.getElementById("drift").checked;
+  let new_troos = document.getElementById("new_troos").checked;
+
+  if (drift === true) {
+    let drift_products = arr.filter(function (el) {
+      var k = el.title;
+      for (var i = 0; i < k.length; k++) {
+        if (k[i] + k[i + 1] + k[i + 2] + k[i + 3] + k[i + 4] === "Drift") {
+          return el;
+        }
+      }
+    })
+    displayProducts(drift_products);
+  }
+  else {
+    displayProducts(arr);
+  }
+}
+
+//Troos checkbox Filter
+
+document.getElementById("new_troos").addEventListener("change", myTroos);
+
+function myTroos() {
+  let new_troos = document.getElementById("new_troos").checked;
+
+  if (new_troos === true) {
+    let troos_products = arr.filter(function (el) {
+      var k = el.title;
+      for (var i = 0; i < k.length; k++) {
+        if (k[i] + k[i + 1] + k[i + 2] === "New") {
+          return el;
+        }
+      }
+    })
+    displayProducts(troos_products);
+  }
+  else {
+    displayProducts(arr);
+  }
+}
+
+//checkBox Sort Based on Size
+
+
+document.getElementById("sizeChart").addEventListener("change", onInput);
+
+function onInput() {
+  let id1 = document.getElementById("6").checked;
+  let id2 = document.getElementById("7").checked;
+  let id3 = document.getElementById("8").checked;
+  let id4 = document.getElementById("9").checked;
+  let id5 = document.getElementById("10").checked;
+  let id6 = document.getElementById("11").checked;
+  let id7 = document.getElementById("12").checked;
+
+
+  if (id1 == true) {
+    let items1 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "6" || k == "9") {
+        return el
+      }
+    })
+    displayProducts(items1);
+  } else if (id2 == true) {
+    let items2 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "8" || k == "10" || k == "6") {
+        return el
+      }
+    })
+    displayProducts(items2);
+  } else if (id3 == true) {
+    let items3 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "7" || k == "11") {
+        return el
+      }
+    })
+    displayProducts(items3);
+  } else if (id4 == true) {
+    let items4 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "12" || k == "13" || k == "15") {
+        return el
+      }
+    })
+    displayProducts(items4);
+  } else if (id5 == true) {
+    let items5 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "14") {
+        return el
+      }
+    })
+    displayProducts(items5);
+  }
+  else if (id6 == true) {
+    let items6 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "6" || k == "10") {
+        return el
+      }
+    })
+    displayProducts(items6);
+  }
+  else if (id7 == true) {
+    let items7 = arr.filter((el) => {
+      let k = el.id;
+      if (k == "9" || k == "15") {
+        return el
+      }
+    })
+    displayProducts(items7);
+  }
+  else {
+    displayProducts(arr);
+  }
+}
