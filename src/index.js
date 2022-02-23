ba4116de5d88c1d19292ab9c16060967e55430db
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-02-23 14:37:51 +0530

    data added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const connect = require("./src/config/db");
+const connect = require("./config/db");
 require("dotenv").config();
-const productsContrller = require("./src/controllers/product.controller");
-
+const productsContrller = require("./controllers/product.controller");
 
 const Port1 = process.env.Port || 1234;
 const app = express();
 app.use(express.json());
 app.use("/products", productsContrller);
 
+// app.set("views", "views");
+app.set("view engine", "ejs");
 
 
-
+app.get("", (req, res)=>{
+  return res.render("product")
+})
 
 app.listen(Port1, async (req, res) => {
   try {
-    connect();
+   await connect();
     console.log(`Connected to the ${Port1}`);
   } catch (error) {
     console.log(error.message);
