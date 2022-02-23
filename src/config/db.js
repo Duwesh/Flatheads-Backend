1b08e251129c63da8e70bbbcc06fcab7aa3f597b
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-02-23 14:23:49 +0530

    landing page and checkout done

--- src/config/db.js
+++ src/config/db.js
@@ -12,3 +12,14 @@ require("dotenv").config();
 //     }
 //   );
 // };
+
+
+module.exports = () => {
+  return mongoose.connect(
+    process.env.MONGODB_URI ||  "mongodb://127.0.0.1:27017/flatheads_test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const passport = require("passport");
-// const flash = require("connect-flash");
-const session = require("express-session");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-// const connect = require("./config/db");
+app.set("view Engine", "ejs");
+
+app.use(express.static("public"));
+
+// all page redirection
+//home pager
+app.get("", async (req, res) => {
+  return res.render("home.ejs");
+});
+
+// //cart page
+// app.get("/cart", async (req, res) => {
+//   return res.render("cart.ejs");
+// });
+
+// //login page
+// app.get("/login", async (req, res) => {
+//   return res.render("login.ejs");
+// });
+
+// //register page
+// app.get("/register", async (req, res) => {
+//   return res.render("register.ejs");
+// });
+
+//checkout page
+app.get("/checkout", async (req, res) => {
+  return res.render("checkout.ejs");
+});
+
+//thankyou page
+app.get("/thankyou", async (req, res) => {
+  return res.render("thankyou.ejs");
+});
+
+const connect = require("./config/db");
 
 app.listen(process.env.PORT || 4095, async () => {
-    try{
-      //   await connect();
-      console.log(`Listening to port ${process.env.PORT}`);
-    }catch(e){
-        console.log("err: ", e.message);
-    }
-
+  try {
+    await connect();
+    console.log(`Listening to port ${process.env.PORT}`);
+  } catch (e) {
+    console.log("err: ", e.message);
+  }
 });
